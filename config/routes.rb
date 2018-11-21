595a83022c5cdefb0d61750f9d4d905e1a7fe07b
Jorge David <jorgedjr21@gmail.com>
2018-11-21 21:25:54 -0200

    Adds the admission index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :students, only: %i[index create update destroy]
+      resources :admissions, only: [:index]
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     end
   end
